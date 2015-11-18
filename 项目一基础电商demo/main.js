dcc09f00d03ff3c4dafcdc7614b89fb1af26e5e8
David_Yu <yuyang545262477@gmail.com>
2015-11-18 17:10:13 +0800

    修改部分代码更加人性化

--- 项目一基础电商demo/main.js
+++ 项目一基础电商demo/main.js
@@ -1,38 +1,31 @@
-//两个目标
-//第一获取屏幕滚动数据，
-//第二，获取每一个div到top的距离。
-//第三，判断scrollTop与setoff的大小。推断翻到了那一页.
 (function($){
     $(document).ready(function(){
         $(window).scroll(function(){
+//            屏幕滚动数据
             var m = $(document).scrollTop();
-//            console.log(m);目标一完成。
-            var mdivs = $('h3');
-            var currentId = '';
             
-            var menu = $('#menu');
             
-            mdivs.each(function(){
+            var lefts = $('.left');
+            lefts.each(function(){
                 var that = $(this);
-                var mdiv = that.offset().top;
-//                console.log(mdiv);任务2完成
-//                获得currentId的值
-                if(m>mdiv){
+                var left = that.offset().top;
+                
+                var currentId = '';
+                if((m+300)>left){
                     currentId = '#'+that.attr('id');
-//                    console.log(currentId);
                 }else{
                     return false;
                 }
-//                获得menu的值
-                var currentLink = menu.find('.active');
-                if(currentId && currentLink.attr('href')!=currentId){
+                
+                var currentLink = $('.right').find('.active');
+                if(currentId && currentId != currentLink.attr('href')){
                     currentLink.removeClass('active');
-                    menu.find('[href='+currentId+']').addClass('active');
+                    $('.right').find('[href='+currentId+']').addClass('active');
                 }
-                
-                
-                
             })
+            
+            
+            
         })
     })
 })(jQuery);
